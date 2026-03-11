b25e065665810eeea0b2db19d6c5180207381a99
JamesRoddy <148975212+JRRoddy@users.noreply.github.com>
2026-03-11 09:43:13 +0000

    add js

---
+++ SkillViewManagement.js
@@ -0,0 +1,81 @@
+// class SkillView
+// {
+    
+//     constructor(skillId,skillGroupTitles, skillNames)
+//     {
+//          this.skillGroupTitles = skillGroupTitles
+//          this.skillNames = skillNames
+//          this.skillId = skillId;
+
+//     }
+
+
+
+// }
+
+
+
+
+
+// class SkillViewManager
+// {
+
+//     constructor(skillViewSections, skillViewSectionItems,skillViewFunctions )
+//     {
+//       this.skillViewFunctions = skillViewFunctions
+//       this.skillViews = new Map();
+  
+//         const skillViewsOnPage = document.getElementsByClassName("project-skills-view");
+        
+//         for(let i = 0; i < skillViewsOnPage.length; i++)
+//         {
+//             let skillView = skillViewsOnPage[i];
+//             if(skillViewFunctions.has(skillView.id))
+//             {
+//                skillViewFunctions.get()(skillViewSections[skillView.id],skillViewSectionItems[skillView.id]);  
+              
+
+//             }
+             
+              
+
+
+//         }
+
+//     }
+
+
+   
+// }
+
+
+
+
+
+ export function parseSkillViews(skillViewSections, skillViewSectionItems )
+ {
+        // const skillViewsOnPage = document.getElementsByClassName("technical-skills-view");
+        // for(let i = 0; i < skillViewsOnPage.length; i++)
+        // {
+            
+        //     let skillView = skillViewsOnPage[i];
+            
+        //     if(skillViewSectionItems.has(skillView.id) && skillViewSections.has(skillView.id))
+        //     {
+        //       let gridItems = skillViewSectionItems.get(skillView.id);
+        //       let sectionTitile = skillViewSections.get(skillView.id);
+        //       for(let i = 0; i< gridItems.length;i++)
+        //       {
+                 
+  
+        //       }
+ 
+        //     }
+      
+             
+              
+
+
+        // }
+
+}
